0ce028be0dc4803d7da3646e6ad41fb68ccb7a13
PrabhuBalu2996 <prabudurio@gmail.com>
2022-08-24 15:11:22 +0530

    New project

---
+++ src/org/demo/Stringconcept.java
@@ -0,0 +1,19 @@
+package org.demo;
+
+public class Stringconcept {
+
+	public static void main(String[] args) {
+		String a = "hello world in java";
+		
+				
+		String[] e = a.split("  ");
+		
+		 a = e[2]+" "+e[1];
+		System.out.println(a);
+		
+		
+		
+		
+		
+	}
+}
